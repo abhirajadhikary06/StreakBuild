ec3a001d3827b9afc798769d8f8eaf060cfd1537
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-02-15 16:39:13 +0530

    Added updates

--- learning_tracker/learning_tracker/settings.py
+++ learning_tracker/learning_tracker/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory to store uploaded file
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
